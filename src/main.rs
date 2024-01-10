873875496583e7cc41fd608a08ecbb0548a6e773
imreMD <imre@o2.pl>
2024-01-10 22:23:10 +0100

    node_builder started

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,6 @@ mod helper_functions;
 
 use std::collections::HashMap;
 
-type Precendence = Int;
-
-
-fn enginneer(lexer_string: String) -> Node {
-
-    // TODO: implement the engineer 
-    // engineer will spawn node_builders
-    // collect effect of node_builder work
-    // build the AST Tree base on information 
-    // from each node_builder 
-
-}
-
-fn node_builder(material: String)-> (Vec<Char>, Precedence) {
-
-    //TODO: implement a node builder 
-    // node_builder will receive a Vec<Char> i.e. ["3","+"]
-    // and will produce a Node (left or right)
-    // node_builder will have an identifier to enable the collection
-    // of their respective work by the engineer
-}
 #[derive(Debug)]
 enum Leaf {
     Variable(String),
